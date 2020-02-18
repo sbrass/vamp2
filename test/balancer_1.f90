f643ec0bdd529d8289df5023aadff891e589aebc
Simon Brass <simon.brass@desy.de>
2020-02-18 16:12:18 +0100

    MPI-fy balancer_1 and add introduce commandline derived type

--- test/balancer_1.f90
+++ test/balancer_1.f90
@@ -1,17 +1,61 @@
-!! Init request balancer.
+!> Test balancer_simple_t implementation.
 program main
   use iso_fortran_env, only: ERROR_UNIT, &
        r64 => REAL64
 
-  use request_balancer
+  use signal, only: signal_print_pid_and_wait
+  use test_utils, only: commandline_t
+
+  use balancer_base
+  use balancer_simple
+
+  use mpi_f08
 
   implicit none
 
-  type(request_balancer_t) :: balancer
+  class(balancer_base_t), allocatable :: balancer
 
-  integer, parameter :: N_WORKERS = 2, &
-       N_RESOURCES = 2
+  integer, parameter :: N_RESOURCES = 4
+  logical, dimension(N_RESOURCES) :: parallel_grid = .false.
+  integer :: worker, handler
+  integer :: rank, n_workers
 
-  call balancer%init (n_workers = n_workers, n_resources = n_resources)
+  type(commandline_t) :: cmd
+
+  call MPI_INIT ()
+  call MPI_COMM_RANK (MPI_COMM_WORLD, rank)
+  call MPI_COMM_SIZE (MPI_COMM_WORLD, n_workers)
+
+  call cmd%parse ()
+  if (cmd%gdb_attach) then
+     if (cmd%gdb_attach_rank > n_workers) &
+          call msg_fatal ("Cannot attach to rank outside of communicator.")
+     if (cmd%gdb_attach_rank == rank) then
+        call signal_print_pid_and_wait ()
+     end if
+     call MPI_BARRIER (MPI_COMM_WORLD)
+  end if
+
+  parallel_grid(3) = .true.
+  worker = rank + 1
+
+  allocate (balancer_simple_t :: balancer)
+  select type (balancer)
+  type is (balancer_simple_t)
+     call balancer%init (n_workers = n_workers, n_resources = n_resources)
+     call balancer%update_state (worker, parallel_grid)
+  end select
   call balancer%write (ERROR_UNIT)
+
+  do while (balancer%is_pending ())
+     if (balancer%is_assignable (worker_id = worker)) then
+        call balancer%assign_worker (worker_id = worker, resource_id = handler)
+        write (ERROR_UNIT, "(A,1X,I0,1X,I0)") "BALANCER", worker, handler
+        call balancer%free_worker(worker_id = worker)
+     end if
+  end do
+
+  call balancer%write (ERROR_UNIT)
+
+  call MPI_FINALIZE ()
 end program main
--- test/test_utils.f90
+++ test/test_utils.f90
@@ -1,12 +1,63 @@
 module test_utils
   use, intrinsic :: iso_fortran_env, only: ERROR_UNIT
 
+  use diagnostics
+  use iterator
+
   implicit none
 
   private
 
-  public :: commandline_is_gdb_attach
+!! Example: --gdb-attach=[rank], -g=[rank]
+
+  type :: commandline_t
+     logical :: gdb_attach = .false.
+     integer :: gdb_attach_rank = 0
+   contains
+     procedure :: write => commandline_write
+     procedure :: parse => commandline_parse
+  end type commandline_t
+
+  public :: commandline_is_gdb_attach, commandline_t
 contains
+  subroutine commandline_write (cmd, unit)
+    class(commandline_t), intent(in) :: cmd
+    integer, intent(in), optional :: unit
+    integer :: u
+    u = ERROR_UNIT; if (present (unit)) u = unit
+    write (ERROR_UNIT, "(A,1X,L1)") "GDB_ATTACH", cmd%gdb_attach
+    write (ERROR_UNIT, "(A,1X,I0)") "GDB_ATTACH_RANK", cmd%gdb_attach_rank
+  end subroutine commandline_write
+
+  subroutine commandline_parse (cmd)
+    class(commandline_t), intent(out) :: cmd
+    type(iterator_t) :: cmd_iter
+    integer :: arg_len, arg_status
+    character(len=256) :: arg
+    character(:), allocatable :: arg_value
+    call cmd_iter%init (1, command_argument_count ())
+    SCAN_CMD: do while (cmd_iter%is_iterable ())
+       call get_command_argument (number = cmd_iter%get_current (), &
+            value = arg, length = arg_len)
+       select case (arg(:arg_len))
+       case ("--gdb-attach", "-g")
+          cmd%gdb_attach = .true.
+          call cmd_iter%next_step ()
+          if (cmd_iter%is_iterable ()) then
+             call get_command_argument (number = cmd_iter%get_current (), &
+                  value = arg, length = arg_len)
+             select case (arg(1:1))
+             case ("-")
+                cycle SCAN_CMD
+             end select
+             print *, "ARG", arg
+             read (arg(:arg_len), *) cmd%gdb_attach_rank
+          end if
+       end select
+       call cmd_iter%next_step ()
+    end do SCAN_CMD
+  end subroutine commandline_parse
+
   logical function commandline_is_gdb_attach () result (flag)
     integer :: i
     character(len=256) :: arg
